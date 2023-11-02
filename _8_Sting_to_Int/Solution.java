0563385e8ff8bcc28beda02b3c114c65489accd7
Fraser McLeod <fdm360@mail.usask.ca>
2023-11-02 11:20:44 -0600

    finish atoi

--- _8_Sting_to_Int/Solution.java
+++ _8_Sting_to_Int/Solution.java
@@ -1,4 +1,4 @@
-package _8_Sting_to_Int;
+
 
 public class Solution {
     public static void main(String args[]){
@@ -7,7 +7,7 @@ public class Solution {
         int expected = -1054;
         int result = atoi(test);
         if (expected != result){
-            System.err.printf("Error in %s. Expected: %d but got %d", testName, expected, result);
+            System.err.printf("Error in %s. Expected: %d but got %d\n", testName, expected, result);
         }
 
         testName = "test 2";
@@ -15,7 +15,7 @@ public class Solution {
         expected = Integer.MAX_VALUE;
         result = atoi(test);
         if (expected != result){
-            System.err.printf("Error in %s. Expected: %d but got %d", testName, expected, result);
+            System.err.printf("Error in %s. Expected: %d but got %d\n", testName, expected, result);
         }
 
         testName = "test 3";
@@ -23,7 +23,7 @@ public class Solution {
         expected = Integer.MIN_VALUE;
         result = atoi(test);
         if (expected != result){
-            System.err.printf("Error in %s. Expected: %d but got %d", testName, expected, result);
+            System.err.printf("Error in %s. Expected: %d but got %d\n", testName, expected, result);
         }
 
         testName = "test 4";
@@ -31,11 +31,74 @@ public class Solution {
         expected = 785643;
         result = atoi(test);
         if (expected != result){
-            System.err.printf("Error in %s. Expected: %d but got %d", testName, expected, result);
+            System.err.printf("Error in %s. Expected: %d but got %d\n", testName, expected, result);
+        }
+
+        testName = "test 4";
+        test = "+1";
+        expected = 1;
+        result = atoi(test);
+        if (expected != result){
+            System.err.printf("Error in %s. Expected: %d but got %d\n", testName, expected, result);
         }
     }
 
     public static int atoi(String s){
-        return 0;
+        // strip leading white space
+        s = s.stripLeading();
+        // set initial values
+        int result = 0;
+        boolean isNeg = false;
+        int index = 0;
+        int limitValue = Integer.MAX_VALUE/10;
+        // test first char is '+' or '-'
+        if (s.length() > 0){
+            if (s.charAt(0) == '-'){
+                isNeg = true;
+                limitValue = Integer.MIN_VALUE/10;
+                index++;
+            } else if (s.charAt(0) == '+'){
+                index++;
+            }
+        }
+        // iterate through
+        while (index < s.length()){
+            if (! isDigit(s.charAt(index))){
+                return result;
+            }
+            int digit = Character.getNumericValue(s.charAt(index));
+            // can we add digit?
+            // we cannot multiply by 10
+            if (result < limitValue && isNeg){
+                System.out.printf("Return MINVALUE with result: %d and digit %d\n", result,digit);
+                return Integer.MIN_VALUE;
+            }
+            // we cannot multiply and add the digit
+            if (result == limitValue && digit > 7 && isNeg){
+                return Integer.MIN_VALUE;
+            }
+            //cannot multiply by 10
+            if (result > limitValue && !isNeg){
+                return Integer.MAX_VALUE;
+            }
+            // cannot multiply by 10 and add digit
+            if (result == limitValue && digit > 7 && !isNeg){
+                return Integer.MAX_VALUE;
+            }
+
+            result = result*10;
+            if (isNeg){
+                result -= digit;
+            } else {
+                result += digit;
+            }
+            index++;
+        }
+
+        return result;
+    }
+
+    public static boolean isDigit(char c){
+        return c >= '0' && c <= '9';
     }
 }
