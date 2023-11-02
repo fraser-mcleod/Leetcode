539287052ea9a0e5eb563d169794999013b18ddd
Fraser McLeod <fdm360@mail.usask.ca>
2023-11-02 12:27:55 -0600

    start 24 - swap nodes

---
+++ _24_Swap_Nodes_in_Pairs/Solution.java
@@ -0,0 +1,14 @@
+package _24_Swap_Nodes_in_Pairs;
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        // swap current with next unless cur is null or next is null
+        // if 0 or 1 nodes return head
+        if (head == null || head.next == null){
+            return head;
+        }
+
+        // now we want to swap until we can't anymore
+
+    }
+}
